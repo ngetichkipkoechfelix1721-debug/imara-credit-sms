cc7e18d5612cdc38c5d0423de5db817b10a92338
ngetichkipkoechfelix1721-debug <ngetichkipkoechfelix1721@gmail.com>
2026-08-10 14:40:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Imara Credit SMS Server is running!");
+  res.send("Imara Credit Pesapal Server is running!");
+});
+
+// Check if Pesapal keys are loaded (without showing them)
+app.get("/config-check", (req, res) => {
+  res.json({
+    consumerKeyLoaded: !!process.env.PESAPAL_CONSUMER_KEY,
+    consumerSecretLoaded: !!process.env.PESAPAL_CONSUMER_SECRET,
+    environment: process.env.PESAPAL_ENVIRONMENT
+  });
 });
 
 const PORT = process.env.PORT || 3000;
